4f8193046f95278fdbe44cf02acd53b59ec7c004
Pery Lemke <pery.lemke@gmail.com>
2023-10-13 15:06:13 -0300

    Change response to JSON

--- handler.py
+++ handler.py
@@ -19,17 +19,36 @@ def hello(event, context):
 def post(event, context):
     req = json.loads(event['body'])
     num_pessoas = req["pessoas"]
+    sendMessage = req["sendSMS"]
     contas = req["contas"]
     resultado = []
     total_valores = 0
     
     for k, v in contas.items():
-        valor_formatado = locale.currency(v['valor']/num_pessoas, grouping=True, symbol=True)
-        resultado.append(f"{v['data_vcto']} - {k.title()}: {valor_formatado}")
+        valor_formatado = locale.currency(
+            v['valor']/num_pessoas, 
+            grouping=True, 
+            symbol=True,
+        )
+        resultado.append({
+            "data_vcto": v['data_vcto'],
+            "conta": k.title(),
+            "valor": valor_formatado
+        })
         total_valores += v['valor']/num_pessoas
         
-    total_formatado = locale.currency(total_valores, grouping=True, symbol=True)
-    msg = "\n".join(resultado)
-    msg += f"\nTotal de Contas: {total_formatado}"
+    total_formatado = locale.currency(
+        total_valores, 
+        grouping=True, 
+        symbol=True
+    )
+    
+    res_data = {
+        "sendSMS": sendMessage,
+        "contas": resultado,
+        "total": total_formatado
+    }
+    
+    response = json.dumps(res_data, ensure_ascii=False)
         
-    return msg
+    return response
